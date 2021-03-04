76d338236a145113b0daa06ed8c593ce46e720a0
budi-kurniawan <budi2020@gmail.com>
2021-03-04 19:17:57 +1100

    r

--- spyrl/agent_builder/impl/d2dspl_actor_critic_traces_agent_builder.py
+++ spyrl/agent_builder/impl/d2dspl_actor_critic_traces_agent_builder.py
@@ -5,14 +5,14 @@ from spyrl.discretiser.discretiser import Discretiser
 from spyrl.normaliser.normaliser import Normaliser
 
 class D2DSPLActorCriticTracesAgentBuilder(AgentBuilder):    
-    def __init__(self, num_actions: int, discretizer: Discretiser, max_num_samples_for_classifier: int, 
-                normalizer: Normaliser, milestone_episodes, hidden_layer_sizes = [300, 300]) -> None:
-        super().__init__(num_actions, discretizer=discretizer, normalizer=normalizer)
+    def __init__(self, num_actions: int, discretiser: Discretiser, max_num_samples_for_classifier: int, 
+                normaliser: Normaliser, milestone_episodes, hidden_layer_sizes = [300, 300]) -> None:
+        super().__init__(num_actions, discretiser=discretiser, normaliser=normaliser)
         self.max_num_samples_for_classifier = max_num_samples_for_classifier
         self.hidden_layer_sizes = hidden_layer_sizes
         self.milestone_episodes = milestone_episodes
 
     @override(AgentBuilder)
     def create_agent(self, seed, initial_policy_path):
-        return D2DSPLActorCriticTracesAgent(self.num_actions, self.discretizer, self.max_num_samples_for_classifier, 
-                self.normalizer, self.milestone_episodes, self.hidden_layer_sizes, seed)
+        return D2DSPLActorCriticTracesAgent(self.num_actions, self.discretiser, self.max_num_samples_for_classifier, 
+                self.normaliser, self.milestone_episodes, self.hidden_layer_sizes, seed)
