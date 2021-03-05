a574229bcd7f0cbad09e226a90d86dead73315b5
budi-kurniawan <budi2020@gmail.com>
2021-03-05 22:54:06 +1100

    r

--- example/lunarlander/lunarlander_d2dspl_testing.py
+++ example/lunarlander/lunarlander_d2dspl_testing.py
@@ -22,8 +22,8 @@ __version__ = "0.1.0"
 if __name__ == '__main__':
     env = gym.make('LunarLander-v2')
     num_actions = env.action_space.n
-    num_learning_episodes = 5000
-    policy_parent_path = 'result/lunarlander/d2dspl-03/'
+    num_learning_episodes = 1000
+    policy_parent_path = 'result/lunarlander/d2dspl-06/'
     out_path = policy_parent_path + 'performance/'
     config = ActivityConfig(num_trials=10, num_episodes=100, out_path=out_path)    
     policy_parent_path = os.path.join(get_project_dir(), policy_parent_path)    
