a06887761ac8f4b99185a57a8ee4098207e6011d
budi-kurniawan <budi2020@gmail.com>
2021-03-07 09:34:02 +1100

    r

--- example/lunarlander/lunarlander_acet_testing.py
+++ example/lunarlander/lunarlander_acet_testing.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     num_learning_episodes = 1000
     policy_parent_path = 'result/lunarlander/acet-21/'
     out_path = policy_parent_path + 'performance-'+ str(num_learning_episodes) + '/'
-    config = ActivityConfig(num_trials=3, num_episodes=100, out_path=out_path)    
+    config = ActivityConfig(start_trial=4, num_trials=7, num_episodes=100, out_path=out_path)    
     policy_parent_path = os.path.join(get_project_dir(), policy_parent_path)
     tester_builder = ActorCriticTesterBuilder(policy_parent_path, num_learning_episodes, LunarLanderDiscretiser24576())
     testing = Testing(listeners=[ConsoleLogListener(), TestResultLogger(RewardType.TOTAL)])
